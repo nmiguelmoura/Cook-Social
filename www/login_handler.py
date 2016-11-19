3164f9b9e6365c313cb779760d9922bfb72ceb60
nmiguelmoura <nmiguelmoura@gmail.com>
2016-11-19 11:14:41 +0000

    Tweak to login file

--- www/login_handler.py
+++ www/login_handler.py
@@ -42,7 +42,7 @@ class LoginHandler(handler.Handler):
         # Id user does not exist or password doesn't match, render page with
         # error message.
         self.render('login.html', username=username,
-                    error_message="Nome de utilizador ou palavra-chave invalida.")
+                    error_message="Nome de utilizador ou palavra-chave inválida.")
 
     def verify_password(self, existing_user, given_password):
         # Compare password posted and stored and return result.
